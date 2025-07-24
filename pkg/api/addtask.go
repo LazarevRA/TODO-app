4ba95c8c2e4425a1442e0f84706354ca31361324
LazarevRA <lazarev.rost.a@gmail.com>
2025-07-25 01:23:33 +0300

    fix JSON errors encoding

--- pkg/api/addtask.go
+++ pkg/api/addtask.go
@@ -11,24 +11,28 @@ import (
 func addTaskHandler(w http.ResponseWriter, r *http.Request) {
 
 	var task db.Task
+	var err error
 	//Декодирование
 	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
 
-		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		writeJSON(w, "error", fmt.Sprintf("incorrect data in JSON: %s", err))
+
 		return
 	}
 
 	//Проверка на наличие заголовка
 	if task.Title == "" {
 
-		http.Error(w, "Title is required", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		writeJSON(w, "error", "title required")
 		return
 	}
 
 	//Проверка даты
 	if err := checkDate(&task); err != nil {
-
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
+		writeJSON(w, "error", fmt.Sprintf("Check date error: %s", err))
 		return
 	}
 
@@ -36,18 +40,19 @@ func addTaskHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := db.AddTask(&task)
 
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Database error: %v", err), http.StatusInternalServerError)
+		w.WriteHeader(http.StatusBadRequest)
+		writeJSON(w, "error", fmt.Sprintf("DB add task error: + %s", err))
 		return
 	}
 	//Запись last ID
 
-	writeJSON(w, map[string]int64{"id": id})
+	writeJSON(w, "id", id)
 
 }
 
-func writeJSON(w http.ResponseWriter, data any) {
+func writeJSON(w http.ResponseWriter, key string, value any) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	json.NewEncoder(w).Encode(data)
+	json.NewEncoder(w).Encode(map[string]any{key: value})
 }
 
 func checkDate(task *db.Task) error {
